ba4816184e921e397440cc2599c4624131b4b106
opsecmaster23 <272116733+whyyounoanon@users.noreply.github.com>
2026-04-09 17:44:50 +0100

    hideyouridentityig.lua

--- utilities/hideyouridentityig.lua
+++ utilities/hideyouridentityig.lua
@@ -1,5 +1,5 @@
 local Players = game:GetService("Players")
-local LocalPlayer = Players.LocalPlayer
+local LocalPlayer = Players.LocalPlayer or Players.PlayerAdded:Wait()
 
 local LocalPlayerName = LocalPlayer.Name
 local LocalPlayerDisplayName = LocalPlayer.DisplayName
@@ -7,26 +7,25 @@ local enableme = 1
 
 bitdancer999 = function()
 	pcall(function()
-		local allSpace = game:GetDescendants()
-		for i = 1, #allSpace do
-			if allSpace[i].ClassName == "TextLabel" then
-				if string.find(allSpace[i].Text, LocalPlayerName) then
-					allSpace[i].Text = string.gsub(allSpace[i].Text, LocalPlayerName, "Bitdancer")
-					allSpace[i].Changed:Connect(function()
-						allSpace[i].Text =
-							string.gsub(allSpace[i].Text, LocalPlayerName, "Bitdancer")
+		local universe = game:GetDescendants()
+		for i = 1, #universe do
+			if universe[i].ClassName == "TextLabel" then
+				if string.find(universe[i].Text, LocalPlayerName) then
+					universe[i].Text = string.gsub(universe[i].Text, LocalPlayerName, "Bitdancer")
+					universe[i].Changed:Connect(function()
+						universe[i].Text = string.gsub(universe[i].Text, LocalPlayerName, "Bitdancer")
 					end)
 				end
-				if string.find(allSpace[i].Text, LocalPlayerDisplayName) then
-					allSpace[i].Text =
-						string.gsub(allSpace[i].Text, LocalPlayerDisplayName, "Bitdancer")
-					allSpace[i].Changed:Connect(function()
-						allSpace[i].Text =
-							string.gsub(allSpace[i].Text, LocalPlayerDisplayName, "Bitdancer")
+
+				if string.find(universe[i].Text, LocalPlayerDisplayName) then
+					universe[i].Text = string.gsub(universe[i].Text, LocalPlayerDisplayName, "Bitdancer")
+					universe[i].Changed:Connect(function()
+						universe[i].Text = string.gsub(universe[i].Text, LocalPlayerDisplayName, "Bitdancer")
 					end)
 				end
 			end
 		end
+
 		game.DescendantAdded:Connect(function(descendant)
 			if descendant.ClassName == "TextLabel" then
 				if string.find(descendant.Text, LocalPlayerName) then
@@ -37,14 +36,13 @@ bitdancer999 = function()
 				end
 			end
 		end)
+
 		game.DescendantAdded:Connect(function(descendant)
 			if descendant.ClassName == "TextLabel" then
 				if string.find(descendant.Text, LocalPlayerDisplayName) then
-					descendant.Text =
-						string.gsub(descendant.Text, LocalPlayerDisplayName, "Bitdancer")
+					descendant.Text = string.gsub(descendant.Text, LocalPlayerDisplayName, "Bitdancer")
 					descendant.Changed:Connect(function()
-						descendant.Text =
-							string.gsub(descendant.Text, LocalPlayerDisplayName, "Bitdancer")
+						descendant.Text = string.gsub(descendant.Text, LocalPlayerDisplayName, "Bitdancer")
 					end)
 				end
 			end
@@ -53,10 +51,10 @@ bitdancer999 = function()
 end
 
 if enableme == 1 then
-    bitdancer999()
-    warn('good boy')
+	bitdancer999()
+	warn("GOOD BOYYY, YOU'RE BITDANCER NOW BLUD")
 elseif enableme == 0 then
-    print('ok nga.')
+	print("FUCK YOU NGAAAAAAAA")
 end
 
 --// Retarded code, please don't mind. Ts might be helpful //--
